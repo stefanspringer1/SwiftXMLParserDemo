afa8d0a460fffd803879a4abf65f95b9a4e018cb
stefanspringer1 <stefan.springer@icloud.com>
2021-07-27 07:47:42 +0200

    changed // "swift-tools-version:5.3" to "// swift-tools-version:5.4.0"; using "executableTarget"; expecting SwiftXMLParser >= 0.1.18

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SwiftXMLParserDemo",
     dependencies: [
-        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "0.1.17"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "0.1.18"),
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "SwiftXMLParserDemo",
             dependencies: ["SwiftXMLParser"]),
         .testTarget(
