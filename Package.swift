ba5e72505cef8d15a998b295d6016bdbec4d3a0d
stefanspringer1 <stefan.springer@icloud.com>
2021-08-21 19:20:16 +0200

    using SwiftXMLParser >= 0.1.46 and swift-tools-version:5.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4.0
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftXMLParserDemo",
     dependencies: [
-        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "0.1.45"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "0.1.46"),
     ],
     targets: [
         .executableTarget(
