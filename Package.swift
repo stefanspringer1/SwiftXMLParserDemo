7212cb07a71edf93b1a1216be82bec3edc0859b8
stefanspringer1 <stefan.springer@icloud.com>
2021-08-22 12:12:28 +0200

    using SwiftXMLParser >= 0.1.48

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftXMLParserDemo",
     dependencies: [
-        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "0.1.47"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "0.1.48"),
     ],
     targets: [
         .executableTarget(
